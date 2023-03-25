a00a31c22a59609d455396bff0e2d292d8b60825
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-03-25 17:31:04 -0300

    Add tabela de concta id dos eventos

--- controller/events.controller.js
+++ controller/events.controller.js
@@ -12,24 +12,103 @@ const firebaseConfig = {
   measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
+const contactIdTable =[
+  {
+    eventCode: "E100",
+    eventDescription: "Emergência Médica"
+  },
+
+  {
+    eventCode: "E130",
+    eventDescription: "Disparo de Intrusão",
+  },
+  {
+    eventCode: "E120",
+    eventDescription: "Disparo Botão de Pânico",
+  },
+  {
+    eventCode: "E121", 
+    eventDescription: "Coação",
+  },
+  {
+    eventCode: "E350", 
+    eventDescription: "Falha de Comunicação",
+  },
+  {
+    eventCode: "E302", 
+    eventDescription: "Falha de Bateria",
+  },
+  {
+    eventCode: "R302",
+    eventDescription: "REstauro da Bateria",
+  },
+  {
+    eventCode: "E301", 
+    eventDescription: "Corte de Energia",
+  },
+  {
+    eventCode: "R301", 
+    eventDescription: "Restauro da Energia",
+  },
+  {
+    eventCode: "E401",   
+    eventDescription: "Sistema Desativado",
+    },
+  {
+    eventCode: "R401", 
+    eventDescription: "Sistema Ativado",
+  },
+  {
+    eventCode: "E602", 
+    eventDescription: "Teste Periódico Recebido",
+  },
+  {
+    eventCode: "E570", 
+    eventDescription: "Zona Anulada",
+  },
+  {
+    eventCode: "EAA0",
+    eventDescription: "Servidor Viaweb Iniciado",
+  },
+  {
+    eventCode: "EAA1", 
+    eventDescription: "Servidor Viaweb Parado",
+  },
+  {
+    eventCode: "EAA6", 
+    eventDescription: "GPRS Off-Line",
+  },
+  {
+    eventCode: "RAA6",
+    eventDescription: "Restauro do GPRS",  
+  }
+];
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const saveEvent = async (request, response) => {
   const bodyData = request.body;
-  console.log(bodyData)
 
-  // TODO: Fazer um loop para validar o tipo/nome do evento => tableContactId
+  let eventDescription = "Evento não definido";
+
+  contactIdTable.forEach((contactId) => {
+    if(contactId.eventCode == bodyData.eventCode) {
+      eventDescription = contactId.eventDescription;
+      console.log(eventDescription)
+    }
+  });
 
   try {
-    const eventRef = await addDoc(collection(db, 'events'), {
+    const eventRef = await addDoc(collection(db, "events"), {
       attendedEvent: bodyData.attendedEvent,
       zoneNumber: bodyData.zoneNumber,
       accountNumber: bodyData.accountNumber,
       eventCode: bodyData.eventCode,
       partitionNumber: bodyData.partitionNumber,
       dateEvent: bodyData.dateEvent,
-      eventTime: bodyData.eventTime,      
+      eventTime: bodyData.eventTime,
+      eventDescription: eventDescription,     
     });
     response.status(200).send({
       eventRef: eventRef.id,
@@ -40,6 +119,5 @@ const saveEvent = async (request, response) => {
     }); 
   }  
 };
-
 
 module.exports = { saveEvent };
